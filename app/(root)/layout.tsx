559aa4adf3929671ecda406ed7edce8d1b39a360
Rusiko13 <rusochlaidze@gmail.com>
2025-02-13 16:32:49 +0400

    add header, footer and theme toggle functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,13 @@
-const RootLayout = ({children,}: Readonly<{children: React.ReactNode}>) => {
-    return ( 
-        <div className="flex h-screen flex-col"> Root
-            <main className="main flex-1 wrapper">{children}</main>
-        </div>
-     );
-}
- 
+import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+	return (
+		<div className="flex h-screen flex-col">
+			<Header></Header>
+			<main className="main flex-1 wrapper">{children}</main>
+			<Footer></Footer>
+		</div>
+	);
+};
+
 export default RootLayout;
