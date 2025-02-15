8f92ac0632909b2a29a4bcc6e8b0016900f20aa9
Rusiko13 <rusochlaidze@gmail.com>
2025-02-15 11:03:19 +0400

    fix: type error root/smug/page.tsx

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -34,7 +34,7 @@ const ProductDetailsPage = async (props: {
 						<div className="flex flex-col sm:flex-row sm:items-center gap-3">
 							<ProductPrice
 								value={Number(product.price)}
-								ClassName="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
+								className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
 							/>
 						</div>
 					</div>
