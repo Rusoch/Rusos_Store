670f446cfe08b87652faebf724d013cdcef45674
Rusiko13 <rusochlaidze@gmail.com>
2025-02-21 16:50:22 +0400

    refactor: update sign-in-form

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,11 +1,27 @@
+'use client'
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
 
 const CredentialsSignInForm = () => {
+	const [data, action] = useActionState(signInWithCredentials, {
+		success: false,
+		message: "",
+	});
+	const SignInButton = () => {
+		const { pending } = useFormStatus();
+		return (
+			<Button disabled={pending} className="w-full" variant="default">
+				{pending ? "Signing In..." : "Sign In"}
+			</Button>
+		);
+	};
 	return (
-		<form>
+		<form action={action}>
 			<div className="space-y-6">
 				<div>
 					<Label htmlFor="email">Email</Label>
@@ -28,13 +44,17 @@ const CredentialsSignInForm = () => {
 					/>
 				</div>
 				<div>
-					<Button className="w-full" variant="default">
-						Sign In
-					</Button>
-                    <div className="text-sm text-center text-muted-foreground mt-5">
-                        Don&apos;t have an account?{' '}
-                        <Link href='/sign-up' target='_self' className='link'>Sign Up</Link>
-                    </div>
+					<SignInButton />
+				</div>
+				{data && !data.success && (
+					<div className="text-center text-destructive">{data.message}</div>
+				)}
+
+				<div className="text-sm text-center text-muted-foreground">
+					Don&apos;t have an account?{" "}
+					<Link href="/sign-up" target="_self" className="link">
+						Sign Up
+					</Link>
 				</div>
 			</div>
 		</form>
