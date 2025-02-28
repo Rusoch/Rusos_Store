ea8ee3a6e3566d7bfcf70700075a384ba9982c09
Rusiko13 <rusochlaidze@gmail.com>
2025-02-28 19:22:56 +0400

    cart page with cart-tables

--- lib/utils.ts
+++ lib/utils.ts
@@ -28,7 +28,7 @@ export function formatError(error: any) {
 		return fieldErrors.join(". ");
 	} else if (
 		error.name === "PrismaClientKnownRequestError" &&
-		error.code === "P2002" 
+		error.code === "P2002"
 	) {
 		//handle Prisma error
 		const field = error.meta?.target ? error.meta.target[0] : "Field";
@@ -43,12 +43,36 @@ export function formatError(error: any) {
 
 // Round number to 2 decimal places
 export function round2(value: number | string) {
-	if (typeof value === 'number') {
-	  return Math.round((value + Number.EPSILON) * 100) / 100;
-	} else if (typeof value === 'string') {
-	  return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
+	if (typeof value === "number") {
+		return Math.round((value + Number.EPSILON) * 100) / 100;
+	} else if (typeof value === "string") {
+		return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
 	} else {
-	  throw new Error('Value is not a number or string');
+		throw new Error("Value is not a number or string");
 	}
-  }
-  
+}
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+	currency: "USD",
+	style: "currency",
+	minimumFractionDigits: 2,
+});
+
+// Format currency using the formatter above
+
+export function formatCurrency(amount: number | string | null) {
+	if (typeof amount === "number") {
+		return CURRENCY_FORMATTER.format(amount);
+	} else if (typeof amount === "string") {
+		return CURRENCY_FORMATTER.format(Number(amount));
+	} else {
+		return "NaN";
+	}
+}
+
+// Format Number
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
+
+export function formatNumber(number: number) {
+	return NUMBER_FORMATTER.format(number);
+}
